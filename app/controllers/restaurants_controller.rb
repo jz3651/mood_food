8c86162f9df31344c8e23919d508b2fc18aa75d1
Nicholas Curran <nic-curran1@hotmail.com>
2021-07-24 11:27:08 +1000

    show

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -5,6 +5,7 @@ class RestaurantsController < ApplicationController
 
   def show
     @restaurant = Restaurant.find(params[:id])
+
     @restaurant_image = ""
     if @restaurant.cuisine == "Mexican"
       @restaurant_image << "https://source.unsplash.com/Y0zbn9lPCEU"
@@ -13,18 +14,6 @@ class RestaurantsController < ApplicationController
     else
       @restaurant_image << "https://source.unsplash.com/Y11iTVE2DFA"
     end
+
   end
-
-  # private
-
-  # def restaurant
-  #   @restaurant_image = ""
-  #   if @restaurant.cuisine == "Mexican"
-  #     @restaurant_image << "https://source.unsplash.com/Y0zbn9lPCEU"
-  #   elseif(@restaurant.cuisine == "Italian")
-  #     @restaurant_image << "https://source.unsplash.com/MqT0asuoIcU"
-  #   else
-  #     @restaurant_image << "https://source.unsplash.com/Y11iTVE2DFA"
-  #   end
-  # end
 end
